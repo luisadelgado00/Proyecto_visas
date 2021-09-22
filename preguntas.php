0a1e0d1b8807783a2e4bd4b43d0abb7fc2f6b08e
luisadelgado00 <luisaferdelbil@hotmail.com>
2021-09-22 18:22:49 -0500

    proyecto visas

--- preguntas.php
+++ preguntas.php
@@ -1,3 +1,4 @@
+
 <?php
 
     $pregunta1 = $_POST['pregunta1'];
@@ -42,6 +43,34 @@ if (($puntos == 0) || ($puntos == 1) || ($puntos == 2)) {
     $mensaje = "Tu probabilidad de obtener una visa es muy alta";
 }
 $porcentaje = ($puntos / 5)  * 100;
-echo "Resultado prueba prediagnóstica: $porcentaje % de probabilidad. <br> $mensaje <br>"
+/*echo '<script language="javascript">alert("Probabilidad de visa: '.$porcentaje.' % "+"'.$mensaje.'");</script>';*/
+/*echo "Resultado prueba prediagnóstica: $porcentaje % de probabilidad. <br> $mensaje <br>"*/
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="styles/preguntas.css" rel="stylesheet" type="text/css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <title>VISAS CONTINENTAL</title>
+</head>
+<body>
+    
+    <h2 class="titulo">RESULTADOS:</h2>
+    <div class="wrap">
+
+    <form action="preguntas.php" class="fomrulario" method="POST">
+    <div class="pregu">
+    <h4>Los resultados de su prediagnóstico son:. </h4>
+    <div class="resultado">
+ <?php
+ echo "$porcentaje % de probabilidad. <br> $mensaje <br>"
+ ?>
+ </div>
+</div>
+</div>
+</body>
+</html>
